550d392d1a1788413fd16ef114f31b1fa586a771
Nicolas Lara <nicolaslara@gmail.com>
2011-02-20 15:17:33 +0100

    settings change

--- settings.py
+++ settings.py
@@ -11,9 +11,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'geodjango',                      # Or path to database file if using sqlite3.
-        'USER': 'mindbug',                      # Not used with sqlite3.
+        'ENGINE': '', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         #'PASSWORD': '',                  # Not used with sqlite3.
         #'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         #'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -96,3 +96,8 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     'world',
 )
+
+try:
+    from local_settings import *
+except ImportError, e:
+    pass
